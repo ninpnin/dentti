c46a1581994b0f2b8cd333fbf2b92805582f14eb
ninpnin <vaino.yrjanainen@aalto.fi>
2019-06-10 23:49:03 +0300

    Remove unused code

--- denttisivu/resources/title-animation.js
+++ denttisivu/resources/title-animation.js
@@ -1,15 +1,10 @@
 $(document).ready(function() {
   $(".title").lettering();
-  $(".button").lettering();
 });
 
 $(document).ready(function() {
   animation();
-}, 1000);
-
-$('.button').click(function() {
-  animation();
-});
+}, 10);
 
 
 function animation() {
