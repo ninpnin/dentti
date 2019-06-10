b35469270aff0f653b67fe90b388b8351a329822
ninpnin <vaino.yrjanainen@aalto.fi>
2019-06-10 22:02:37 +0300

    Add 300ms latency for PDF generation

--- denttisivu/resources/pdf-saver.js
+++ denttisivu/resources/pdf-saver.js
@@ -1,11 +1,16 @@
+function convertTable() {
+	var doc = new jsPDF();
+	doc.autoTable({html: '#pricetable'});
+  	doc.save('hinnasto.pdf');
+  	console.log("DONE!")
+}
+
 function downloadPrices() {
 	// Check whether user wants to download the price list
 	var r = confirm("Ladataanko hinnasto?");
 	if (r == true) {
 		console.log("Write table...");
-		var doc = new jsPDF();
-		doc.autoTable({html: '#pricetable'});
-	  	doc.save('hinnasto.pdf');
-	  	console.log("DONE!")
+	  	
+	  	setTimeout(function() { convertTable(); } , 300);
 	}
 }
