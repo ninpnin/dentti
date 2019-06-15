e4dad46e8f16340a9338166386f59483a9313b04
ninpnin <vaino.yrjanainen@aalto.fi>
2019-06-15 13:49:05 +0300

    Add alternative image for narrow screens

--- resources/image-carousel.js
+++ resources/image-carousel.js
@@ -3,19 +3,25 @@ function switchImage() {
 	var img1 = "img/nalle-odottaa.jpg"
 	var img2 = "img/hammasharja.jpg"
 	var img3 = "img/cerec.jpg"
+	var img4 = "img/hammasharja2.jpg"
 
 	// Randomize; TODO: make deterministic
 	// TODO: adjust placing differently for each image
 	var random_boolean = Math.random() >= 0.666;
-	if (random_boolean) {
-		console.log("nalle");
-		$("#teddyimage").css({"background-image": "url("+img1+")"});
-	} else if (Math.random() >= 0.50) {
-		console.log("harja");
-		$("#teddyimage").css({"background-image": "url("+img2+")"});
+	if ($( document ).width() < 1081) {
+		console.log("harja 2");
+		$("#teddyimage").css({"background-image": "url("+img4+")"});
 	} else {
-		console.log("cerec");
-		$("#teddyimage").css({"background-image": "url("+img3+")"});
+		if (random_boolean) {
+			console.log("nalle");
+			$("#teddyimage").css({"background-image": "url("+img1+")"});
+		} else if (Math.random() >= 0.50) {
+			console.log("harja");
+			$("#teddyimage").css({"background-image": "url("+img2+")"});
+		} else {
+			console.log("cerec");
+			$("#teddyimage").css({"background-image": "url("+img3+")"});
+		}
 	}
 
 	// Set 8 second timeout for changing the image
